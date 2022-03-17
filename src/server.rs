8449a9067908c8ee95656b5f169dbaa9e8fd0d10
gabrielnsd <gabrielnsdantas@gmail.com>
2022-03-17 14:31:34 -0300

    sync commit

---
+++ src/server.rs
@@ -0,0 +1,53 @@
+use crate::http::{Request, Response, StatusCode};
+use std::convert::TryFrom;
+use std::convert::TryInto;
+use std::io::{Read, Write};
+use std::net::TcpListener;
+
+pub struct Server {
+    addr: String,
+}
+
+impl Server {
+    pub fn new(addr: String) -> Self {
+        Self { addr: addr }
+    }
+
+    pub fn run(self) {
+        println!("Listening on {}", self.addr);
+
+        let listener = TcpListener::bind(&self.addr).unwrap();
+
+        loop {
+            match listener.accept() {
+                Ok((mut stream, _)) => {
+                    let mut buffer = [0; 1024];
+                    match stream.read(&mut buffer) {
+                        Ok(_) => {
+                            println!("Received a request: {:?}", String::from_utf8_lossy(&buffer));
+                            let response = match Request::try_from(&buffer[..]) {
+                                Ok(request) => {
+                                    dbg!(request);
+                                    Response::new(
+                                        StatusCode::Ok,
+                                        Some("<h1> HI! </h1>".to_string()),
+                                    )
+                                }
+                                Err(e) => {
+                                    println!("Failed to pass a request! {}", e);
+                                    Response::new(StatusCode::BadRequest, None)
+                                }
+                            };
+
+                            if let Err(e) = response.send(&mut stream) {
+                                println!("Failed to send response: {}", e);
+                            }
+                        }
+                        Err(e) => println!("Failed to read from connection: {}", e),
+                    }
+                }
+                Err(e) => println!("Failed to estalish a connection: {}", e),
+            }
+        }
+    }
+}
